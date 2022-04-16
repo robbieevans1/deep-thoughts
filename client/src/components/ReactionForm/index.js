ffb21704b554e6f5f4e744c8a6db04d88b456742
robcabobb <robcabobb@gmail.com>
2022-04-15 23:40:06 -0400

    added script for heroku

--- client/src/components/ReactionForm/index.js
+++ client/src/components/ReactionForm/index.js
@@ -61,5 +61,6 @@ const ReactionForm = ({ thoughtId }) => {
     </div>
   );
 };
-
+BtQZAfmVscYsGY74
+mongodb+srv://evansrm5:BtQZAfmVscYsGY74@cluster0.r716t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
 export default ReactionForm;
